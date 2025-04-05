db86f5a9f9bc7110a66dfdb4c64658824a08bc55
Miguelitox36 <miguelitox36@hotmail.com>
2025-04-05 13:53:19 -0500

    Project

---
+++ TDV-Semana-2-Tarea1/TDV-Semana-2-Tarea1/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDV_Semana_2_Tarea1
+{
+    internal class Player
+    {
+    }
+}
